1aa66465a6a15d5ddf2ab62b7b3db13f57fe8b77
KhalilAllah Dimassi <dimassi0khalil@gmail.com>
2025-06-10 17:59:51 +0100

    fixed icons + icon "my profile" page

--- src/pages/K-Profile/content/Competence/ProfilePage.tsx
+++ src/pages/K-Profile/content/Competence/ProfilePage.tsx
@@ -3,6 +3,7 @@ import { isAuthenticated } from "../../../../utils/jwt";
 import GuestMode from "./content/GuestMode";
 import OnlineMode from "./content/OnlineMode";
 import JobOpportunities from "./content/JobOpportunities";
+import CompetenceSVG from "../../SidebarIcons/CompetenceSVG";
 
 const KProfile = ({ onClose }: { onClose: () => void }) => {
   const [isOnline] = useState(isAuthenticated);
@@ -10,7 +11,11 @@ const KProfile = ({ onClose }: { onClose: () => void }) => {
   return (
     <div className="w-full">
       <div className="flex items-center space-x-3 my-4">
-        <h1 className="text-xl font-semibold bg-[#297280] bg-clip-text text-transparent">
+        <CompetenceSVG
+          className={`w-8 h-8 md:w-4 md:h-4 lg:w-8 lg:h-8 transition-all duration-500 `}
+          color="#297280"
+        />
+        <h1 className="text-xl font-semibold text-black">
           Mon Profil
         </h1>
       </div>
