1fb824114851568cf7d76bf647c8a5f28b5daeae
KhalilAllah Dimassi <dimassi0khalil@gmail.com>
2025-07-12 23:58:04 +0100

    sidebar indicator bug fix

--- src/pages/K-Profile/Sidebar.tsx
+++ src/pages/K-Profile/Sidebar.tsx
@@ -54,41 +54,25 @@ const Sidebar = ({ onIconClick, defaultSelected, horizontal, setHorizontal }: Si
       {/* Sidebar Icons */}
       <div className={`flex ${horizontal
         ? "flex-row gap-x-40 justify-center items-center"
-        : "flex-col gap-20 py-12 items-center"}`}>
-
+        : "flex-col gap-20 py-12 items-center"}`}
+      >
         {icons.map(({ id, Icon }) => (
           <div
             key={id}
-            className={`relative cursor-pointer flex items-center ${horizontal ? "p-2" : ""}`}
+            className={`cursor-pointer flex items-center ${horizontal ? "p-2" : ""}`}
             onClick={() => handleIconClick(id)}
           >
-            {/* Selection Indicator (Only for Vertical Mode) */}
-            {!horizontal && activeIcon === id && (
-              <div
-                className="absolute bg-white rounded-full"
-                style={{
-                  width: "4rem",
-                  height: "2.5rem",
-                  border: "3px solid rgba(140, 214, 194, 0.47)",
-                  boxShadow: "1px 2px 8px #a5d8ca",
-                }}
-              />
-            )}
-
             <div
-              className="relative z-10 transition-transform duration-300"
-              style={{
-                transform: horizontal
-                  ? "translateY(0px)"
-                  : activeIcon === id
-                    ? "translateX(14px)"
-                    : "translateX(0px)",
-              }}
+              className={`transition-all duration-300 ${!horizontal && activeIcon === id
+                ? "bg-white rounded-full py-2 px-4 ml-6"
+                : ""}`}
             >
-              <Icon className={`w-8 h-8 md:w-4 md:h-4 lg:w-8 lg:h-8 transition-all duration-300
-                   ${activeIcon === id
+              <Icon
+                className={`w-8 h-8 md:w-4 md:h-4 lg:w-8 lg:h-8 transition-colors duration-300 ${activeIcon === id
                   ? "text-[#297280]"
-                  : "text-white hover:text-gray-200"}`} />
+                  : "text-white hover:text-gray-200"
+                  }`}
+              />
             </div>
           </div>
         ))}
